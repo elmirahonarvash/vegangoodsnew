712f9e4193c97385af0b7cb797fa2a6522b88cdb
elmira <elmira_honarvash@yahoo.com>
2020-04-17 11:25:00 -0700

    updated product

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
+  contactusForm: any;
 
   ngOnInit() {
+    this.contactusForm = this.fb.group({
+      emailAddress: "",
+      // number: "",
+    });
   }
-
+}
+export class FormFieldPrefixSuffixExample {
+  hide = true;
 }
